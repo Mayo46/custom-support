e8023d8b019ad78b70dbece3e7129c51878d77ba
salman_nazir <salman.nazir@alliedc.com>
2022-10-15 19:50:19 +0500

    setup Vue 3 Project and design Navbar for PC Laptop screen

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+</script>
+
+<template>
+    <Navbar/>
+    <router-view/>
+    <HelloWorld/>
+</template>
+
+<style lang="scss">
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   color: #2c3e50;
+// }
+
+// html,
+// body {
+//   margin: 0;
+//   padding: 0;
+//   width: 100%;
+//   height: 100%;
+// }
+</style>
